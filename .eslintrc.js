d3997b9fbcc0271fcee14a8f0dc82d6296b9addc
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-04 14:52:54 -0300

    Projeto finalizado

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'standard',
+    'prettier'
+  ],
+
+  plugirns: [
+    'prettier'
+
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      }
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    camelcase: 'off',
+    "prettier/prettier": "error",
+  },
+}
---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+    secret:'3b2613ff007c695c2d560d0e9c9ccbcf',
+    expiresIn:'2d'
+}
