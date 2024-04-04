9ae268d0c3017b57262077093ddf5cb19509f61b
fabiosena1436 <fabiosena1436@gmail.com>
2024-04-04 16:39:04 -0300

    ALTERAÇAO DO ROUTES.JS E SERVER

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import multer from "multer";
-import multerConfig from './config/multer'
+import multerConfig from "./config/multer";
 
 import ProductController from "./app/controllers/ProductController";
 import SessionController from "./app/controllers/SessionController";
@@ -8,27 +8,29 @@ import CategoryController from "./app/controllers/CategoryController";
 import UserController from "./app/controllers/UserController";
 import OrderController from "./app/controllers/OrderController";
 
-import authMiddleware from './app/middlewares/auth'
+import authMiddleware from "./app/middlewares/auth";
 
-const upload = multer(multerConfig)
+const upload = multer(multerConfig);
 
 const routes = new Router();
 
-
+routes.get("/", (req, res) => {
+  return res.json({ message: "Hello to my first API" });
+});
 
 routes.post("/users", UserController.store);
 
 routes.post("/sessions", SessionController.store);
 
-routes.use(authMiddleware)
+routes.use(authMiddleware);
 
-routes.post("/products", upload.single('file'), ProductController.store);
+routes.post("/products", upload.single("file"), ProductController.store);
 routes.get("/products", ProductController.index);
-routes.put("/products/:id", upload.single('file'), ProductController.update);
+routes.put("/products/:id", upload.single("file"), ProductController.update);
 
-routes.post("/categories", upload.single('file'), CategoryController.store);
+routes.post("/categories", upload.single("file"), CategoryController.store);
 routes.get("/categories", CategoryController.index);
-routes.put("/categories/:id", upload.single('file'), CategoryController.update);
+routes.put("/categories/:id", upload.single("file"), CategoryController.update);
 
 routes.post("/orders", OrderController.store);
 routes.put("/orders/:id", OrderController.update);
