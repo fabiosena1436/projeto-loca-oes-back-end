33bc9d4497ee9bd3aac767d9e155f774ffcf168e
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2024-04-05 00:43:24 +0000

    Erro multer

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,7 +1,7 @@
-import multer from "multer";
+import {multer} from "multer";
 import { v4 } from "uuid";
 import { extname, resolve } from 'path'
-import { request } from "http";
+
 
 export default {
     storage: multer.diskStorage({
@@ -11,16 +11,3 @@ export default {
         },
     }),
 }
-
-// import multer from "multer";
-// import { v4 } from "uuid";
-// import { extname, resolve } from 'path'
-
-// export default {
-//     storage: multer.diskStorage({
-//         destination: resolve(__dirname, '..', '..', 'uploads'),
-//         filename: (request, file, callback) => {
-//             return callback(null, v4() + extname(file.originalname))
-//         },
-//     }),
-// }
