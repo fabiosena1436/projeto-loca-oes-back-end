6deb5f16caefe7d0b7ca9c11d26e1a983a0ec4b5
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2024-04-05 00:58:21 +0000

    Routes e server

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import multer from "multer.js";
+import multer from "multer";
 import multerConfig from "./config/multer";
 
 import ProductController from "./app/controllers/ProductController";
@@ -8,7 +8,7 @@ import CategoryController from "./app/controllers/CategoryController";
 import UserController from "./app/controllers/UserController";
 import OrderController from "./app/controllers/OrderController";
 
-import authMiddleware from "./app/middlewares/auth.js";
+import authMiddleware from "./app/middlewares/auth";
 
 const upload = multer(multerConfig);
 
